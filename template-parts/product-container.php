fc2c9bb2f46fb3682e961538d2c4a9dbb1ecab83
Rau261977 <141036372+Rau261977@users.noreply.github.com>
2024-11-01 21:32:09 -0300

    ok

--- template-parts/product-container.php
+++ template-parts/product-container.php
@@ -1,15 +1,49 @@
 <div class="container pt-2">
     <hr class="border">
+    <div class="d-flex justify-content-center align-items-center">
+        <h3 class="card-text-title text-center mt-2 pb-3">MOSTRADOR</h3>
+    </div>
+
+    <h4 class="text-left tit-cont">
+        <a href="/categoria/carne-vacuna">
+            Carne vacuna <span style="font-size: 1.4rem;">(ver más...)</span>
+        </a>
+    </h4>
+    <div class="d-flex justify-content-center align-items-center">
+        <?php echo do_shortcode('[products category="carne-vacuna" limit="5"]'); ?>
+    </div>
+
+
+
+    <h4 class="text-left tit-cont">
+        <a href="/categoria/carne-vacuna">
+            Cerdo <span style="font-size: 1.4rem;">(ver más...)</span>
+        </a>
+    </h4>
+
 
     <div class="d-flex justify-content-center align-items-center">
-        <h3 class="card-text-title text-center mt-2 pb-5">MOSTRADOR</h3>
+        <?php echo do_shortcode('[products category="cerdo" limit="5"]'); ?>
     </div>
-    <?php echo do_shortcode('[products category="carne-vacuna" limit="5"]'); ?>
-    <?php echo do_shortcode('[products category="cerdo" limit="5"]'); ?>
-    <?php echo do_shortcode('[products category="pollo" limit="5"]'); ?>
+
+
+
+
+
+    <h4 class="text-left tit-cont">
+        <a href="/categoria/carne-vacuna">
+            Pollo <span style="font-size: 1.4rem;">(ver más...)</span>
+        </a>
+    </h4>
+    <div class="d-flex justify-content-center align-items-center">
+        <?php echo do_shortcode('[products category="pollo" limit="5"]'); ?>
+    </div>
 
 </div>
 
 <style>
-
+    .tit-cont {
+        margin: 0 0 -1rem 5rem;
+        font-size: 2rem;
+    }
 </style>
